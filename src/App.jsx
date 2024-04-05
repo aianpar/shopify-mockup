a46353097712a08070ff1a03696c852dcc18e104
Aian Parnikov <phunkpar@gmail.com>
2024-04-05 11:59:10 -0400

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Orders from "/src/page/Orders/Orders.jsx"
+import "./App.css";
 
 function App() {
-
   return (
     <>
-   
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Orders/>}></Route>
+          <Route path="/edit"></Route>
+
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
